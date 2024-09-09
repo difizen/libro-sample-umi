22020313492a5087561efa821d1c804dc1f289f0
sunshinesmilelk <1176136681@qq.com>
2024-09-09 13:56:57 +0800

    chore: update config

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "index" },
+    { path: "/", component: "libro-lab/index" },
     { path: "/docs", component: "docs" },
   ],
   npmClient: 'pnpm',
+  // 引入
+  plugins: ['@difizen/umi-plugin-mana'],
+  // 配置
+  mana: {
+    decorator: true,
+    nodenext: true,
+    routerBase: true,
+    runtime: true,
+  },
 });
